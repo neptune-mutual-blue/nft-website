6d8b8a83de4c27ada2d2688bfdb6d2c6b064314e
flashburst <82953782+flashburst@users.noreply.github.com>
2023-03-27 11:08:59 +0530

    Updated settings and workflow

--- src/views/home/PremiumNfts.jsx
+++ src/views/home/PremiumNfts.jsx
@@ -1,10 +1,11 @@
-import NftCardWithBlurEffect from '@/components/NftCardWithBlurEffect/NftCardWithBlurEffect'
+import NftCardWithBlurEffect
+  from '@/components/NftCardWithBlurEffect/NftCardWithBlurEffect'
 import { Slider } from '@/components/Slider/Slider'
 
 function PremiumNfts ({ premiumNfts }) {
   return (
     <div className='premium nfts'>
-      <Slider gap={24}>
+      <Slider gap={16}>
         {
         premiumNfts.map((character) => {
           return (
