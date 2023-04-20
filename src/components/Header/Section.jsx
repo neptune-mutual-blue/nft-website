2ad85f8ae23dedf6e1d863f390202338343ebb30
srmasharad <srmasharad49@gmail.com>
2023-04-20 16:54:35 +0545

    fix: linting issue on section.jsx

--- src/components/Header/Section.jsx
+++ src/components/Header/Section.jsx
@@ -11,54 +11,47 @@ const Section = ({ section }) => {
       <h4 className='title'>{title}</h4>
       <div className='content'>
         {
-          links && links.map((link) => (
-            link.isExternal
-              ? <a
-                  key={
-              link.title
-            }
-                  className='item container'
-                  href={
-              normalizeUrl(link.href)
-            }
-                  target='_blank'
-                  rel='noreferrer'
-                  data-include-theme={link.includeTheme ? true : undefined}
-                >
+          links && links.map((link) => link.isExternal
+            ? (
+              <a
+                key={link.title}
+                className='item container'
+                href={normalizeUrl(link.href)}
+                target='_blank'
+                rel='noreferrer'
+                data-include-theme={link.includeTheme ? true : undefined}
+              >
                 {link.icon && <Icon size='md' variant={link.icon} />}
                 <div>
                   <h3 className='title'>
                     {link.title}
                     {link?.badge && <div className='badge'>{link.badge}</div>}
                     {/* {link.isExternal && (
-                    <Icon size='sm' variant='link-external-01' />
-                  )}{' '} */}
+                        <Icon size='sm' variant='link-external-01' />
+                      )}{' '} */}
                   </h3>
                   <p className='description'>{link.text}</p>
                 </div>
-                </a>
-              : <Link
-                  key={
-              link.title
-            }
-                  className='item container'
-                  href={
-                normalizeUrl(link.href)
-              }
-                >
+              </a>)
+            : (
+              <Link
+                key={link.title}
+                className='item container'
+                href={normalizeUrl(link.href)}
+              >
                 {link.icon && <Icon size='md' variant={link.icon} />}
                 <div>
                   <h3 className='title'>
                     {link.title}
                     {link?.badge && <div className='badge'>{link.badge}</div>}
                     {/* {link.isExternal && (
-                    <Icon size='sm' variant='link-external-01' />
-                  )}{' '} */}
+                      <Icon size='sm' variant='link-external-01' />
+                    )}{' '} */}
                   </h3>
                   <p className='description'>{link.text}</p>
                 </div>
-                </Link>
-          ))
+              </Link>)
+          )
         }
       </div>
     </div>
